eb77063c90d9db06265e4ca8e53cf3e411081344
realitix <realitix@gmail.com>
2017-09-28 23:11:07 -0700

    Add Windows compilation support

--- _cffi_build/vma_build.py
+++ _cffi_build/vma_build.py
@@ -10,7 +10,7 @@ LINUX = platform.system() == 'Linux'
 # ----------
 # BUILD STATIC LIB
 # ----------
-def build():
+def build_linux():
     p1 = path.join(HERE, 'include')
     p2 = path.join(HERE, 'vk_mem_alloc.h')
     p3 = path.join(HERE, 'vk_mem_alloc.o')
@@ -19,10 +19,26 @@ def build():
     cmd1 = 'g++ -std=c++11 -fPIC -x c++ -I' + p1 + c + '-c ' + p2 + ' -o ' + p3
     cmd2 = 'ar rvs ' + p4 + ' ' + p3
 
-    # Launch command
+    call(cmd1, shell=True)
+    call(cmd2, shell=True)
+
+
+def build_windows():
+    p1 = path.join(HERE, 'include')
+    p2 = path.join(HERE, 'vk_mem_alloc.h')
+    p3 = path.join(HERE, 'vk_mem_alloc.obj')
+    p4 = path.join(HERE, 'vk_mem_alloc.lib')
+    c = ' /DVMA_IMPLEMENTATION /DVMA_STATIC_VULKAN_FUNCTIONS=0 '
+    cmd1 = 'CL.exe /c /I' + p1 + c +' /nologo /W3 /WX /Ox /Oi /GF /Gm- /EHsc /MD /GS /Gy /Zc:inline /Zc:wchar_t /Gd /TP /errorReport:none /Fo' + p3 + ' ' + p2
+    cmd2 = 'lib.exe /OUT:' + p4 + ' ' + p3
+
     call(cmd1, shell=True)
     call(cmd2, shell=True)
 
 
 if __name__ == '__main__':
-    build()
+    build_platform = {
+        'Linux': build_linux,
+        'Windows': build_windows
+    }
+    build_platform[platform.system()]()
