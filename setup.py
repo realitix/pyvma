31cf251dc0196e18d98118c62ffa7a13287d0357
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2017-12-26 10:17:01 +0100

    Refactor pyvma architecture

--- setup.py
+++ setup.py
@@ -1,13 +1,52 @@
 from distutils.command.build import build
 from setuptools import setup
+from os import path
+import platform
+from subprocess import call
 
-from _cffi_build import vma_build
 
-
+# ----------
+# BUILD STATIC LIB
+# ----------
 class VmaBuild(build):
     def run(self):
-        vma_build.build()
+        self.build()
         super(VmaBuild, self).run()
+
+    def build_linux(self):
+        p1 = path.join(self.p, 'include')
+        p2 = path.join(self.p, 'vk_mem_alloc.h')
+        p3 = path.join(self.p, 'vk_mem_alloc.o')
+        p4 = path.join(self.p, 'libvk_mem_alloc.a')
+        c = ' -DVMA_IMPLEMENTATION -D_DEBUG -DVMA_STATIC_VULKAN_FUNCTIONS=0 '
+        cmd1 = 'g++ -std=c++11 -fPIC -x c++ -I' + p1 + c + '-c ' + p2 + ' -o ' + p3  # noqa
+        cmd2 = 'ar rvs ' + p4 + ' ' + p3
+
+        call(cmd1, shell=True)
+        call(cmd2, shell=True)
+
+    def build_windows(self):
+        p1 = path.join(self.p, 'include')
+        p2 = path.join(self.p, 'vk_mem_alloc.h')
+        p3 = path.join(self.p, 'vk_mem_alloc.obj')
+        p4 = path.join(self.p, 'vk_mem_alloc.lib')
+        c = ' /DVMA_IMPLEMENTATION /DVMA_STATIC_VULKAN_FUNCTIONS=0 '
+        cmd1 = 'CL.exe /c /I' + p1 + c +' /nologo /W3 /WX /Ox /Oi /GF /Gm- /EHsc /MD /GS /Gy /Zc:inline /Zc:wchar_t /Gd /TP /errorReport:none /Fo' + p3 + ' ' + p2  # noqa
+        cmd2 = 'lib.exe /OUT:' + p4 + ' ' + p3
+
+        call(cmd1, shell=True)
+        call(cmd2, shell=True)
+
+    def build(self):
+        self.p = path.dirname(path.realpath(__file__))
+        self.p = path.join(self.p, 'pyvma')
+        self.p = path.join(self.p, 'pyvma_build')
+        build_platform = {
+            'Linux': self.build_linux,
+            'Windows': self.build_windows
+        }
+        build_platform[platform.system()]()
+
 
 # version is [vma_version].[pyvma_version]
 
@@ -18,7 +57,7 @@ setup(
     author_email="realitix@gmail.com",
     description="Python CFFI binding for Vulkan Memory Allocator",
     long_description="Python CFFI binding for Vulkan Memory Allocator",
-    packages=['_cffi_build', 'pyvma'],
+    packages=['pyvma'],
     install_requires=["cffi"],
     setup_requires=["cffi"],
     include_package_data=True,
@@ -34,5 +73,5 @@ setup(
     license="Apache 2.0",
     ext_package="pyvma",
     cmdclass={'build': VmaBuild},
-    cffi_modules=["_cffi_build/pyvma_build.py:ffibuilder"]
+    cffi_modules=["pyvma/pyvma_build/pyvma_build.py:ffibuilder"]
 )
