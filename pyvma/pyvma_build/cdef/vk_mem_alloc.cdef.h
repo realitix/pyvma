890d342bed0a554426d73edc704b4d4dd971893e
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-01-08 13:34:40 +0100

    remove types from cdef

--- pyvma/pyvma_build/cdef/vk_mem_alloc.cdef.h
+++ pyvma/pyvma_build/cdef/vk_mem_alloc.cdef.h
@@ -1,24 +1,4 @@
-typedef long unsigned int size_t;
-typedef unsigned char __u_char;
-typedef unsigned short int __u_short;
-typedef unsigned int __u_int;
-typedef unsigned long int __u_long;
-typedef signed char __int8_t;
-typedef unsigned char __uint8_t;
-typedef signed short int __int16_t;
-typedef unsigned short int __uint16_t;
-typedef signed int __int32_t;
-typedef unsigned int __uint32_t;
-typedef signed long int __int64_t;
-typedef unsigned long int __uint64_t;
-typedef __int8_t int8_t;
-typedef __int16_t int16_t;
-typedef __int32_t int32_t;
-typedef __int64_t int64_t;
-typedef __uint8_t uint8_t;
-typedef __uint16_t uint16_t;
-typedef __uint32_t uint32_t;
-typedef __uint64_t uint64_t;
+
 typedef struct VkDeviceMemory_T *VkDeviceMemory;
 typedef struct VkPhysicalDevice_T* VkPhysicalDevice;
 typedef struct VkBuffer_T *VkBuffer;
